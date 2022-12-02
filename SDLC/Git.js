d6518ee74f5d15988ce79006ac4175f0545569d0
kapil.raghuwanshi <kapil.raghuwanshi5@gmail.com>
2022-12-02 22:02:46 +0530

    added git commands and gitIgnore file

---
+++ SDLC/Git.js
@@ -0,0 +1,43 @@
+
+// run on terminals or consoles in your computer 
+
+// git version
+
+// git config --global user.name "kapilraghuwanshi"
+// git config --global user.email "kapil.raghuwanshi5@gmail.com"
+
+// git config --global --list
+
+// mkdir test-repo - creating local repo
+
+// cd test-repo 
+
+// git init - initiailizing local repo with git
+
+//git status - to check current status of git
+
+// git add . - add all changes to git
+
+// git commit -m "added 2 files"
+
+// git log
+
+// connect remote repo to local repo
+
+// git remote add origin https://github.com/kapilraghuwanshi/test-repo.git
+
+// git branch -M main
+
+// git push -u origin main
+
+// if your remote has some changes
+
+// git fetch
+
+// git pull https://github.com/kapilraghuwanshi/test-repo main --allow-unrelated-histories 
+
+// git push
+
+// git checkout -b <branch-name>
+
+// git branch -a
